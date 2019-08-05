3124382282aba163a99f1ed6f398073e5354e405
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-08-05 18:44:25 -0400

    Move handling timout away from Timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Timer from './components/Timer';
 import './App.css';
 
 function App() {
 
+  const [over, setOver] = useState(false);
+
+  if (over) {
+    return (
+      <div className="App">
+        <h3>Game over!</h3>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <Timer title="Suhhhhh" duration={10} secPrecision={1} msPrecision={2}/>
+      <Timer title="Suhhhhh" duration={10} secPrecision={1} msPrecision={2} onTimeout={() => setOver(true)}/>
     </div>
   );
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,10 +2,7 @@ import React, {useState} from 'react';
 import useTimer from '../hooks/useTimer'
 
 export default function Timer(props) {
-
-    const [expired, setExpired] = useState(false);
-
-    const {title, duration, secPrecision, msPrecision} = props;
+    const {title, duration, secPrecision, msPrecision, onTimeout} = props;
 
     let t = new Date();
     t.setSeconds(t.getSeconds() + duration);
@@ -17,7 +14,7 @@ export default function Timer(props) {
         start,
         pause,
         restart
-    } = useTimer({expiryTimestamp, onExpire:() => setExpired(true)});
+    } = useTimer({expiryTimestamp, onExpire:() => onTimeout()});
 
     const formatTime = (time, precision=2) => {
         if (precision === 1){
@@ -30,22 +27,15 @@ export default function Timer(props) {
         return time.toString();
     }
 
-    const display = expired ?
-        <div>Time's Up!</div> : 
-        (
-            <div><span>{formatTime(secs, secPrecision)}</span>:<span>{formatTime(miliSecs, msPrecision)}</span></div>
-        ); 
-
     return (
         <div>
             <h1>{title || 'Timer'}</h1>
-            <div style={{fontSize: '100px'}}>
-                {display}
+            <div>
+                <span>{formatTime(secs, secPrecision)}</span>:<span>{formatTime(miliSecs, msPrecision)}</span>
             </div>
             <button onClick={start}>Start</button>
             <button onClick={pause}>Pause</button>
             <button onClick={() => {
-                setExpired(false);
                 let t = new Date();
                 t.setSeconds(t.getSeconds() + duration);
                 restart(t);

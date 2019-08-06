4df6c32dcaa931cc71f98ac1c9917e83298c26ef
Ngoc Nguyen <ngocnguyen9995@gmail.com>
2019-08-06 17:39:15 -0400

    add highscore display to gameinfo

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default function GameInfo(props){
-    const {score, difficulty} = props;
+    const {score, difficulty, highscore} = props;
 
     return (
         <div className="game-info">
             <div className="score">Score: {score}</div>
             <div className="level">Level: {difficulty}</div>
+            <div className="highscore">Highscore: {highscore}</div>
         </div>
     );
 }
